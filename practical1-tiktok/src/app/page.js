606770c27b33512cc7c9b28319a0ab336fa6e37b
Sonam Zangmo <sonamzangmo@Sonam-Zangmo.local>
2025-04-15 15:19:08 +0600

    update practical1

--- practical1-tiktok/src/app/page.js
+++ practical1-tiktok/src/app/page.js
@@ -1,9 +1,11 @@
-import VideoFeed from '@/components/ui/VideoFeed';
+import VideoFeed from '../components/ui/VideoFeed';
 
 export default function Home() {
-    return (
-        <div className="py-4">
-            <VideoFeed />
-        </div>
-    );
+  return (
+    <div className="py-4">
+            <VideoFeed feedType="forYou" />
+    </div>
+  );
 }
+
+
